404e40ef424988677180278702193af73e7ceb28
alphakilimanjaro <s190614@student.dtu.dk>
2020-03-02 15:36:49 +0100

    Lektion 5. CopyFile.

---
+++ src/src/Lektion5/Serializable/SerializeDemo.java
@@ -0,0 +1,21 @@
+package Lektion5.Serializable;
+
+import java.io.*;
+public class SerializeDemo {
+
+    public static void main(String [] args) {
+        Complex c = new Complex(1.0, 2.0);
+
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("C:\\Users\\s190614\\complex.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(c);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved to file");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
